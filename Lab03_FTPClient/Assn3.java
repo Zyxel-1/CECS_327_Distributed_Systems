b5fda082246a4b4bb9035a68141d10687e83668d
Roberto Sanchez <rsanchez92@live.com>
2018-02-14 11:51:10 -0800

    Login Successful

--- Lab03_FTPClient/Assn3.java
+++ Lab03_FTPClient/Assn3.java
@@ -5,8 +5,35 @@
  Assignment: Create a FTP Client to connect to existing
              FTP server.
 */
-public class Assn3{
-  public static void main(String[] args){
-    System.out.println("Hello World");
-  }
+//------------------------------------------------
+import java.io.IOException;
+import org.apache.commons.net.ftp.FTPClient;
+
+public class Assn3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		FTPClient ftp = new FTPClient();
+		try {
+			// Setting up connection to server
+			String server = "13.57.240.111";
+			ftp.connect(server);
+			// Passing Credentials through
+			boolean login = ftp.login("cecs327", "cecs327");
+			// Did I login?
+			if(login) {
+				System.out.println("Login Successful");
+			}else {
+				System.out.println("Login Failed");
+			}
+			System.out.println("Connected to " + server + ".");
+			System.out.print("Reply String: "+ ftp.getReplyString());
+			System.out.println("Reply Code: "+ ftp.getReplyCode());
+			
+		}catch(IOException e)
+		{
+			System.out.println("Something went wrong.");
+		}
+		
+	}
 }
