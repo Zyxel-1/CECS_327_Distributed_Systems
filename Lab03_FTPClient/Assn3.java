a9c56261c31582e7cd607328fe07215e17fa054a
Roberto Sanchez <rsanchez92@live.com>
2018-02-14 10:27:55 -0800

    Creating Assignment3

---
+++ Lab03_FTPClient/Assn3.java
@@ -0,0 +1,12 @@
+/*
+ Name: Roberto Sanchez
+ Date: Feb 13, 18
+ Class: CECS 475 Distributed Systems
+ Assignment: Create a FTP Client to connect to existing
+             FTP server.
+*/
+public class Assn3{
+  public static void main(String[] args){
+    System.out.println("Hello World");
+  }
+}
