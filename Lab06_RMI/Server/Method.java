d60c506366edeca31de21eab5f237a332b6926b3
Roberto Sanchez <Zyxel-1@users.noreply.github.com>
2018-03-20 12:33:24 -0700

    Update Method.java

--- Lab06_RMI/Server/Method.java
+++ Lab06_RMI/Server/Method.java
@@ -10,7 +10,7 @@ import java.rmi.server.*;
 public class Method extends UnicastRemoteObject
          implements MethodInterface {
  
-      public Method () throws RemoteException {   }
+      public Method () throws RemoteException { super(1100)  }
     public int fibonacci(int n) throws RemoteException{
         if(n <= 1){
               return n;
