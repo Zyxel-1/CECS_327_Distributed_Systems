2ff4539f02171ba96ef5796d845909109b4aae7e
Roberto Sanchez <Zyxel-1@users.noreply.github.com>
2018-03-20 12:35:09 -0700

    Update Assn6Server.java

--- Lab06_RMI/Server/Assn6Server.java
+++ Lab06_RMI/Server/Assn6Server.java
@@ -10,6 +10,7 @@ import java.rmi.server.*;
 public class Assn6Server {
 	   public static void main (String[] argv) {
 		   try {
+			   System.SetProperty("java.rmi.server.hostname",argv[0]);
 			   Method localMethods = new Method();			   		   
 			   Naming.rebind("rmi://"+argv[0]+"/cecs327", localMethods);
  
